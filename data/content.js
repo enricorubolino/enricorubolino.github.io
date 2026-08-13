eef9d773da8d5a592d7e1a4b60a2ae299f50cbe8
Enrico Rubolino <168624036+enricorubolino@users.noreply.github.com>
2026-08-13 14:56:16 +0200

    Update PhD description in content.js

--- data/content.js
+++ data/content.js
@@ -5,7 +5,7 @@ window.SITE_CONTENT = {
     "affiliations": [
       "Assistant Professor (tenure track, 2025-), CREST, Institut Polytechnique de Paris",
       "Research Fellow (2021-), University of Lausanne",
-      "PhD at the University of Essex, 2021",
+      "PhD in Economics, University of Essex, 2021",
       "Affiliations: CESifo, ROCKWOOL Foundation"
     ],
     "email": "enrico.rubolino@hotmail.it",
